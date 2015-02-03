42227da898e52199897f21de5cd68205aff11a3b
cyriclavaud <cyril.clavaud@gmail.com>
2015-02-03 14:37:48 +0100

    first commit

---
+++ resources/build_resources.bat
@@ -0,0 +1,2 @@
+echo "building user interfaces..."
+\\VSERVER01\shotgun\Python27_x64\Scripts\pyside-uic.exe -o \\VSERVER01\shotgun\shotgun\install\dev\tk-framework-adminui\python\ui\setup_project.py \\VSERVER01\shotgun\shotgun\install\dev\tk-framework-adminui\resources\setup_project.ui
